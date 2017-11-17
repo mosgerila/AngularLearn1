e9a43afd91fc2530f48939b113d50f6d7c2cd906
mosgerila <calin.turcanu1@gmail.com>
2017-11-17 18:11:09 +0200

    FirstSteps

--- learn/src/app/app.module.ts
+++ learn/src/app/app.module.ts
@@ -1,6 +1,10 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
+import {BrowserAnimationsModule} from '@angular/platform-browser/animations';
+import {MaterialModule} from '@angular/material';
+import {FlexLayoutModule} from '@angular/flex-layout';
 
+import 'hammerjs';
 
 import { AppComponent } from './app.component';
 
@@ -10,7 +14,11 @@ import { AppComponent } from './app.component';
     AppComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    BrowserAnimationsModule,
+    MaterialModule,
+    FlexLayoutModule
+
   ],
   providers: [],
   bootstrap: [AppComponent]
